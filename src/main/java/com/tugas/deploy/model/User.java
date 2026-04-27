99d191b65e1301c7a43f99a1565c5da40c95d4c2
annisadiann <annisadian0412@gmail.com>
2026-04-27 20:08:29 +0700

    update isi file User.java

--- src/main/java/com/tugas/deploy/model/User.java
+++ src/main/java/com/tugas/deploy/model/User.java
@@ -1,14 +1,23 @@
 package com.tugas.deploy.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
 @Data
+@Builder
+@Table(name = "20230140149_users")
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
+    @Id
+    private String id;
     private String nama;
     private String nim;
-    private String jenisKelamin;
+    private String jenis_kelamin;
 }
